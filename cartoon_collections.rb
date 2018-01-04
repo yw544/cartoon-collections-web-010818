a4fac1acd0be15d61cfe8b5162c95f9f1064b38f
York Wang <yw544@nyu.edu>
2018-01-04 22:12:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,14 @@ def long_planeteer_calls(calls)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types. do |cheese|
+    if cheeses.include?(cheese)
+      return cheese
+    else
+      return nil
+
 end
 end
