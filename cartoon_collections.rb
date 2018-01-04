3733b63051039a1c58168a8f495b82713ba7ad51
York Wang <yw544@nyu.edu>
2018-01-04 22:15:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  # code an argument here
   # Your code here
+  dwarves.each_with_index do |name, index|
+    puts "#{index+1} #{name}"
+end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(summon)# code an argument here
   # Your code here
+  summon.collect do |elements|
+    elements.capitalize<<"!"
+
+
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find do |cheese|
+    cheeses.include?(cheese)
+
+end
 end
